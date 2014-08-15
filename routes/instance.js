00cdaab82ace72c9208ca987490ad4393cc65d52
n10o <0x0082@gmail.com>
2014-08-15 00:29:51 +0000

    Oops

--- routes/instance.js
+++ routes/instance.js
@@ -69,7 +69,6 @@ router.get('/start/:id', function(req, res) {
             ],
             LoadBalancerName: lbname
           }
-          console.log("ID,lbname" + id + lbname);
           new AWS.ELB().deregisterInstancesFromLoadBalancer(params, function(err, data){
             if (err){
               console.log(err, err.stack);
